84d247567d03c307e950798368fc662a6358c15a
Khrystyna Kubik <khrystyna.grebeniuk.dev@gmail.com>
2023-12-05 22:30:15 +0100

    Day 2 part 2 refactored

--- day-02/day-2-1-code.js
+++ day-02/day-2-1-code.js
@@ -11,11 +11,11 @@ function readFile(file, callback) {
 }
 
 readFile('./input-data-test.txt', (finishedGames) => {
-    const arrayOfTruesAndFalses = formatInputSplitIntoGames(finishedGames);
-    findMaxOfColor(arrayOfTruesAndFalses);
+    const arraysOfColors = formatInputSortColors(finishedGames);
+    console.log(findMaxOfColor(arraysOfColors));
 });
 
-function formatInputSplitIntoGames(finishedGames) {
+function formatInputSortColors(finishedGames) {
     return finishedGames
         .replace(/[;:]/g, ',')
         .split('\n')
@@ -27,24 +27,30 @@ function formatInputSplitIntoGames(finishedGames) {
                 el = el.split(' ')
                 if (index) {
                     if (el[1] === 'red') {
-                        red.push(+el[0])
+                        pushToColor(red, +el[0])
                     }
                     else if (el[1] === 'green') {
-                        green.push(+el[0])
+                        pushToColor(green, +el[0])
                     }
                     else if (el[1] === 'blue') {
-                        blue.push(+el[0])
+                        pushToColor(blue, +el[0])
                     }
                 }
-                else {
-                    return el;
-                }
             })
-            return [red,green,blue]
+            return [red, green, blue]
         });
 }
 
+function pushToColor(arr, el) {
+    arr.push(el)
+}
+
 function findMaxOfColor(data) {
-    let res = data.map(game => game.map(color => Math.max(...color)).reduce((el, acc) => el * acc)).reduce((el,acc) => el + acc);
-    console.log(res);
+    return data.map(game => game.map(color => findMaxElOfColorArray(color))
+        .reduce((el, acc) => el * acc))
+        .reduce((el, acc) => el + acc);
+}
+
+function findMaxElOfColorArray(arr) {
+    return Math.max(...arr);
 }
