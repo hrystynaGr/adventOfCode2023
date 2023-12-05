32af70985da374f452018f97a0d2a68c29f4e569
Khrystyna Kubik <khrystyna.grebeniuk.dev@gmail.com>
2023-12-05 21:36:35 +0100

    Refactored

--- day-02/day-2-code.js
+++ day-02/day-2-code.js
@@ -11,38 +11,65 @@ function readFile(file, callback) {
 }
 
 readFile('./input-data.txt', (finishedGames) => {
-    let res = finishedGames
+    const arrayOfTruesAndFalses = formatInputSplitIntoGames(finishedGames);
+    zeroForWrongIndexForCorrect(arrayOfTruesAndFalses);
+});
+
+function formatInputSplitIntoGames(finishedGames) {
+    return finishedGames
         .replace(/[;:]/g, ',')
         .split('\n')
         .map(game => {
-            return game.split(', ').map((entry, index) => {
-                if (index) {
-                    const [number, color] = entry.split(' ');
-                    if (color === 'red') return number <= 12;
-                    else if (color === 'green') return number <= 13;
-                    else if (color === 'blue') return number <= 14;
-                }
-                else {
-                    return entry;
-                }
-            });
+            return divideToColorNumberPairs(game);
         });
-    checkFinalResult(res);
-    // console.log(res);
-});
+}
 
-function checkFinalResult(data) {
+function divideToColorNumberPairs(game) {
+    return game.split(', ').map((entry, index) => {
+        return modifyColorNumberPairs(entry, index)
+    });
+}
+
+function modifyColorNumberPairs(entry, index) {
+    entry = entry.split(' ');
+    if (index) {
+        return isCorrectNumberOfColor(entry);
+    }
+    else {
+        return entry[1];
+    }
+}
+
+function isCorrectNumberOfColor([number, color]) {
+    if (color === 'red') return isCorrectNumberOfRed(number);
+    else if (color === 'green') return isCorrectNumberOfGreen(number);
+    else if (color === 'blue') return isCorrectNumberOfBlue(number);
+}
+
+function isCorrectNumberOfRed(number) {
+    return number <= 12;
+}
+
+function isCorrectNumberOfGreen(number) {
+    return number <= 13;
+}
+
+function isCorrectNumberOfBlue(number) {
+    return number <= 14;
+}
+
+function zeroForWrongIndexForCorrect(data) {
     const res = data.map(game => {
         if (game.some(el => el === false)) {
             return 0;
         }
         else {
-            return game[0].split(' ')[1];
+            return +game[0];
         }
     })
-    calculateSum(res);
+    calculateSumOfIndexes(res);
 }
 
-function calculateSum(data) {
-    console.log(data.reduce((el, acc) => +el + +acc))
+function calculateSumOfIndexes(data) {
+    console.log(data.reduce((el, acc) => el + acc))
 }
